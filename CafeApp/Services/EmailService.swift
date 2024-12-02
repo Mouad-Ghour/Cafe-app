1f8c620b9396d7351b8a02707936c0352c63f04d
MouadGhour <mouadghourdou18@gmail.com>
2024-12-02 12:09:57 +0100

    Added emailService and finished the second view, the ordersummary view

--- CafeApp/Services/EmailService.swift
+++ CafeApp/Services/EmailService.swift
@@ -5,4 +5,62 @@
 //  Created by Ghourdou Mouad on 02/12/2024.
 //
 
-import Foundation
+import UIKit
+import MessageUI
+
+class EmailService: NSObject, MFMailComposeViewControllerDelegate {
+    
+    static let shared = EmailService()
+    
+    private override init(){}
+    
+    func sendOrderEmail(
+        drinkType: String,
+        size: String,
+        extras: [String],
+        totalPrice: Double,
+        from viewController: UIViewController
+    ){
+        guard MFMailComposeViewController.canSendMail() else {
+            showMailError(from: viewController)
+            return
+        }
+        
+        let mailComposer = MFMailComposeViewController()
+        mailComposer.mailComposeDelegate = self
+        mailComposer.setToRecipients(["coffee-m2sime@univ-rouen.fr"])
+        
+        let extrasDescription = extras.isEmpty ? "No extras" : extras.joined(separator: ", ")
+        mailComposer.setSubject("\(size) \(drinkType) with \(extrasDescription)")
+
+        let body = """
+        Order Summary:
+        - Drink: \(size) \(drinkType)
+        - Extras: \(extrasDescription)
+        - Total Price: $\(String(format: ".2f", totalPrice))
+        """
+        mailComposer.setMessageBody(body, isHTML: false)
+
+        viewController.present(mailComposer, animated: true, completion: nil)
+    }
+    
+    private func showMailError(from viewController: UIViewController){
+        let alert = UIAlertController(
+            title: "Error",
+            message: "Unable to send mail. Please ensure email is set up on your device.",
+            preferredStyle: .alert
+        )
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        viewController.present(alert, animated: true, completion: nil)
+    }
+    
+    func mailComposeController(
+        _ controller : MFMailComposeViewController,
+        didFinishWith result: MFMailComposeResult,
+        error: Error?
+    ){
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
--- CafeApp/Views/OrderSummary.swift
+++ CafeApp/Views/OrderSummary.swift
@@ -5,4 +5,48 @@
 //  Created by Ghourdou Mouad on 02/12/2024.
 //
 
-import Foundation
+import SwiftUI
+import MessageUI
+
+struct OrderSummaryView: View {
+    
+    @ObservableObject var viewModel: DrinkViewModel
+    
+    var body: some View{
+        VStack{
+            Text("Order Summary")
+                .font(.title)
+            
+            Text("\(viewModel.selectedSize.rawValue) \(viewModel.selectedDrink.rawValue)")
+            if viewModel.extras.isEmpty{
+                Text("No extras")
+            } else {
+                ForEach(viewModel.extras, id:\.self) { extra in
+                        Text(extra)
+                }
+            }
+            
+            Text("Total: $\(viewModel.total, specifier: "%.2f")")
+            
+            Button(action: sendEmail){
+                Text("Order now for $\(viewModel.total, specifier: "%.2f")")
+            }
+            .padding()
+        }
+    }
+    
+    private func sendEmail(){
+        let drinkType = viewModel.selectedDrink.rawValue
+        let size = viewModel.selectedSize.rawValue
+        let extras = viewModel.extras
+        let totalPrice = viewModel.total
+        
+        EmailService.shared.sendOrderEmail(
+            drinkType: drinkType,
+            size: size,
+            extras: extras,
+            totalPrice: totalPrice,
+            from: UIApplication.shared.windows.first?.rootViewController ?? UIViewController()
+        )
+    }
+}
